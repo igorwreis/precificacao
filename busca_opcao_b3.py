5c04dc611a4c827a56f94680a31009e07cfd8943
igorwreis <68671323+igorwreis@users.noreply.github.com>
2021-05-19 16:37:51 -0300

    Update busca_opcao_b3.py

--- busca_opcao_b3.py
+++ busca_opcao_b3.py
@@ -21,14 +21,10 @@ def busca_codigo(a):
     """
 
     # INSTALAR BIBLIOTECAS ABAIXO NO AMBIENTE A SER UTILIZADO !
-    # pip install bs4
     # pip install selenium
     # pip install webdriver-manager
 
-    from getpass import getuser
     from time import sleep
-    import pandas as pd
-    from bs4 import BeautifulSoup as bs
     from selenium import webdriver
     from webdriver_manager.chrome import ChromeDriverManager
     from selenium.webdriver.support.ui import WebDriverWait
